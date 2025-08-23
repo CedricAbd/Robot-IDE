b3197266a3078bda7c287a84d88aab7a1ee750d6
cedric <cedric.abdelbaki@etu.inp-n7.fr>
2025-08-23 11:00:38 +0200

    Add the main TypeScript file

---
+++ src/angular-frontend/src/main.ts
@@ -0,0 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+/**
+ * Bootstraps the Angular application with
+ * AppComponent and applies the provided appConfig.
+ */
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
