d0305f5d65664ca09371507bca35cce9e60b2d70
cedric <cedric.abdelbaki@etu.inp-n7.fr>
2025-08-23 14:51:36 +0200

    Add LanguageRegistrationService

---
+++ src/angular-frontend/src/app/editors/services/language-registration.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { languages } from 'monaco-editor';
+import { monarchLanguage } from '../../core/models/application-consts.model';
+
+/**
+ * Registers the Robot Framework language.
+ * 
+ * Guarantees a unique registration.
+ */
+@Injectable({ providedIn: 'root' })
+export class LanguageRegistrationService {
+  /**
+   * Creates the LanguageRegistrationService instance.
+   * 
+   * Registers the Robot Framework language and its
+   * Monarch tokenizer.
+   */
+  constructor() {
+    languages.register({ id: 'robot-framework' });
+    languages.setMonarchTokensProvider('robot-framework', monarchLanguage)
+  }
+}
